9da8a10a261476b4d027269cad0570fee2bbed3c
Sergey Vasilyan <yegress110@gmail.com>
2026-02-04 22:26:12 +0400

    Description:
        - Update toggle to be animated.

--- src/ui/toggle_switch.py
+++ src/ui/toggle_switch.py
@@ -2,54 +2,97 @@
 
 from typing import override
 
-from PySide6.QtCore import QRect, QSize, Qt
+from PySide6.QtCore import (
+    Property,
+    QEasingCurve,
+    QPoint,
+    QPropertyAnimation,
+    QRect,
+    QSize,
+    Qt,
+    Signal,
+    Slot,
+)
 from PySide6.QtGui import QColor, QPainter, QPaintEvent
-from PySide6.QtWidgets import QCheckBox, QWidget
+from PySide6.QtWidgets import QAbstractButton, QWidget
 from src.utils.common import Colors
 
 
-class ToggleSwitch(QCheckBox):
-    """Modern Toggle Switch in WWM style."""
+class ToggleSwitch(QAbstractButton):
+    """Modern Toggle Switch."""
+
+    __position_changed: Signal = Signal(float)
 
     def __init__(self, parent: QWidget|None=None) -> None:
         """Initialize toggle."""
         super().__init__(parent=parent)
-        self.setChecked(False)
+        self.setCheckable(True)
+        self.setCursor(Qt.CursorShape.PointingHandCursor)
         self.__checked_color: QColor = Colors.ACCENT_1.value.qcolor
         self.__unchecked_color: QColor = Colors.ACCENT_2.value.qcolor
         self.__knob_color: QColor = QColor("#FFFFFF")
-        self.__knob_size: int = 18
-        self.__knob_offset: int = 2
-        self.setChecked(False)
-        self.setStyleSheet("""
-            QCheckBox {
-                spacing: 8px;
-            }
-        """)
+        self.__width: int = 45
+        self.__height: int = int(self.__width * .52)
+        self.__offset: int = 2
+        self.__knob_size: int = self.__height - (self.__offset * 2)
+        self.__position: float = self.__offset
+        self.setMinimumSize(self.__width, self.__height)
+        self.__animation: QPropertyAnimation = QPropertyAnimation(self, b"position", self)
+        self.__animation.setDuration(250)
+        self.__animation.setEasingCurve(QEasingCurve.Type.InOutCubic)
+        self.toggled.connect(self.__start_animation)
+
+    @Property(float, notify=__position_changed)
+    def position(self) -> float:
+        """Return current position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value: float) -> None:
+        """Update position."""
+        value = max(self.__offset, min(self.__width - self.__knob_size - self.__offset, value))
+        if value == self.__position:
+            return
+        self.__position = value
+        self.__position_changed.emit(value)
+        self.update()
 
     @override
     def sizeHint(self) -> QSize:
         """Override size hint."""
-        return QSize(40, 25)
+        return QSize(self.__width, self.__height)
+
+    @override
+    def hitButton(self, pos: QPoint, /) -> bool:
+        """Override hitButton."""
+        return self.rect().contains(pos)
 
     @override
     def paintEvent(self, _event: QPaintEvent) -> None:
         """Override paint event."""
         painter: QPainter = QPainter(self)
-        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing, True)
         track_rect: QRect = QRect(0, 0, self.width(), self.height())
         painter.setBrush(self.__checked_color if self.isChecked() else self.__unchecked_color)
         painter.setPen(Qt.PenStyle.NoPen)
-        radius: int = self.height() // 2
+        radius: int = self.__width // 4
         painter.drawRoundedRect(track_rect, radius, radius)
-        knob_x: int = self.__knob_offset
-        if self.isChecked():
-            knob_x = self.width() - self.__knob_size - self.__knob_offset
-        knob_rect: QRect = QRect(knob_x, ((self.height() // 2) - (self.__knob_size // 2)),
-                                 self.__knob_size, self.__knob_size)
+        painter.setPen(QColor(0, 0, 0, 30))
         painter.setBrush(self.__knob_color)
-        painter.drawEllipse(knob_rect)
+        painter.drawEllipse(QRect(int(self.__position), self.__offset, self.__knob_size,
+                                  self.__knob_size))
         painter.end()
+
+    @Slot()
+    def __start_animation(self, /) -> None:
+        """Start animation.."""
+        self.__animation.stop()
+        self.__animation.setStartValue(self.__position)
+        if self.isChecked():
+            self.__animation.setEndValue(self.__width - self.__knob_size - self.__offset)
+        else:
+            self.__animation.setEndValue(self.__offset)
+        self.__animation.start()
 
 if "__main__" == __name__:
     ...
